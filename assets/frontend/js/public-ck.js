2bd921ffe93b489252a23adf2149306e40bd8afa
Bryce <brycead@gmail.com>
2014-11-17 13:27:53 +0700

    Conditional for ingredients #4

--- assets/frontend/js/public-ck.js
+++
@@ -1,1 +0,0 @@
-(function(e){"use strict";e(function(){})})(jQuery);
--- templates/single/ingredients.php
+++ templates/single/ingredients.php
@@ -4,58 +4,59 @@
  *
  * @package   Recipe Hero
  * @author    Captain Theme <info@captaintheme.com>
- * @version 	  0.8.2
+ * @version   1.0.0
  */
 
 if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 
 global $post;
-$ingredients = get_post_meta( $post->ID, '_recipe_hero_ingredients_group', true ); ?>
+$ingredients = get_post_meta( $post->ID, '_recipe_hero_ingredients_group', true );
 
-<!-- if ( isset ( $ingredients[0]['_recipe_hero_ingredient_quantity'] ) ) { -->
+// Check if any ingredients exist
+if ( ( $ingredients[0]['quantity'] ) ) { ?>
 
-<div class="recipe-single-ingredients">
+	<div class="recipe-single-ingredients">
 
-	<h4 class="inredients-heading">
-		<?php _e( 'Ingredients', 'recipe-hero' ); ?>
-	</h4>
+		<h4 class="inredients-heading">
+			<?php _e( 'Ingredients', 'recipe-hero' ); ?>
+		</h4>
 
-	<ul class="ingredients-list">
+		<ul class="ingredients-list">
 
-	<?php foreach ( (array) $ingredients as $key => $ingredient ) {
+		<?php foreach ( (array) $ingredients as $key => $ingredient ) {
 
-	    $ingredient_quantity = $ingredient_amount = $ingredient_name = '';
+		    $ingredient_quantity = $ingredient_amount = $ingredient_name = '';
 
-	    if ( isset( $ingredient['quantity'] ) && isset( $ingredient['amount'] ) ) {
+		    if ( isset( $ingredient['quantity'] ) && isset( $ingredient['amount'] ) ) {
 
-	        $ingredient_quantity = $ingredient['quantity'];
+		        $ingredient_quantity = $ingredient['quantity'];
 
-	        $ingredient_amount_pre = $ingredient['amount'];
-	    	
-	    	$ingredient_amount = recipe_hero_output_single_ingredient_amount( $ingredient_amount_pre, $ingredient_quantity );
-	  	
-	  	}
+		        $ingredient_amount_pre = $ingredient['amount'];
+		    	
+		    	$ingredient_amount = recipe_hero_output_single_ingredient_amount( $ingredient_amount_pre, $ingredient_quantity );
+		  	
+		  	}
 
-	    if ( isset( $ingredient['name'] ) ) {  
+		    if ( isset( $ingredient['name'] ) ) {  
 
-	        $ingredient_name = $ingredient['name'];
-	        
-	    }
+		        $ingredient_name = $ingredient['name'];
+		        
+		    }
 
-	   	?>
+		   	?>
 
-	   	<li class="ingredients-item" itemprop="ingredients">
-	   		<?php echo $ingredient_amount; ?> 
-	   		<?php echo $ingredient_name; ?> 
-	    </li>
+		   	<li class="ingredients-item" itemprop="ingredients">
+		   		<?php echo $ingredient_amount; ?> 
+		   		<?php echo $ingredient_name; ?> 
+		    </li>
 
-	<?php
-	}
-	?>
-	</ul>
+		<?php
+		}
+		?>
+		</ul>
 
-<hr class="recipe-single-seperator" />
+	<hr class="recipe-single-seperator" />
 
-</div><!-- .recipe-single-ingredients -->
+	</div><!-- .recipe-single-ingredients -->
 
-<!-- } -->
+<?php }
