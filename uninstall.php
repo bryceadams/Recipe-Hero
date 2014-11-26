3f2958113b0f93dfaf28f830cc6b62aa4c502263
Bryce <brycead@gmail.com>
2014-11-27 00:17:04 +0700

    Update uninstall.php

--- uninstall.php
+++ uninstall.php
@@ -14,3 +14,17 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly
 }
 
+$options = array(
+	'recipe_hero_recipes_page_id',
+	'recipe_hero_recipe_order',
+	'recipe_single_image_size',
+	'recipe_steps_image_size',
+	'recipe_thumbnail_image_size',
+	'recipe_hero_enable_lightbox',
+);
+
+foreach ( $options as $option ) {
+	if ( get_option( $option ) ) {
+		delete_option( $option );
+	}
+}
